b20c2e328e6465467277666f5fa68635a79d4a34
zhaodefang <zhaodefang@gmail.com>
2019-11-22 10:34:47 +0800

    1.1
    增加了网站关键字过滤

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            ChangeClipboardChain(this.Handle, nextClipboardViewer);
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox1.Location = new System.Drawing.Point(71, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(591, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(668, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "增加";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 80);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(759, 328);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(696, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "删除";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(668, 14);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 16);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "使用代理";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "视频地址";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.PeachPuff;
+            this.textBox2.ForeColor = System.Drawing.Color.Blue;
+            this.textBox2.Location = new System.Drawing.Point(71, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(591, 21);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "下载方式";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "YoutubeSaver 1.1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.IO;
+
+
+
+namespace WindowsFormsApp1
+{
+
+    public partial class Form1 : Form
+    {
+        #region Definitions
+        //Constants for API Calls...
+        private const int WM_DRAWCLIPBOARD = 0x308;
+        private const int WM_CHANGECBCHAIN = 0x30D;
+
+        //Handle for next clipboard viewer...
+        private IntPtr mNextClipBoardViewerHWnd;
+
+        //API declarations...
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static public extern IntPtr SetClipboardViewer(IntPtr hWndNewViewer);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static public extern bool ChangeClipboardChain(IntPtr HWnd, IntPtr HWndNext);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+
+ 
+        public static extern int SendMessage(IntPtr hWnd, int msg, int wParam, int lParam);
+        #endregion
+
+        IntPtr nextClipboardViewer;
+        public Form1()
+        {
+            InitializeComponent();
+            nextClipboardViewer = (IntPtr)SetClipboardViewer( (IntPtr)this.Handle );
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public enum ShowWindowCommands : int
+        {
+            SW_HIDE = 0,
+            SW_SHOWNORMAL = 1,    //用最近的大小和位置显示，激活
+            SW_NORMAL = 1,
+            SW_SHOWMINIMIZED = 2,
+            SW_SHOWMAXIMIZED = 3,
+            SW_MAXIMIZE = 3,
+            SW_SHOWNOACTIVATE = 4,
+            SW_SHOW = 5,
+            SW_MINIMIZE = 6,
+            SW_SHOWMINNOACTIVE = 7,
+            SW_SHOWNA = 8,
+            SW_RESTORE = 9,
+            SW_SHOWDEFAULT = 10,
+            SW_MAX = 10
+        }
+        [DllImport("shell32.dll")]
+        public static extern IntPtr ShellExecute(
+        IntPtr hwnd,
+        string lpszOp,
+        string lpszFile,
+        string lpszParams,
+        string lpszDir,
+        ShowWindowCommands FsShowCmd
+        );
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddUrl(textBox2.Text.ToString());
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //ShellExecute(IntPtr.Zero, "open", "cmd",@"/c ping www.126.com -t", null, ShowWindowCommands.SW_SHOWNORMAL);
+            string ss = listBox1.Text;
+            if (ss.Length == 0) return;
+            listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            string proxystr = " --proxy 127.0.0.1:1080";
+            string url = textBox2.Text.ToString();
+            if (url.Length == 0) return;
+            if (checkBox1.Checked)
+            {
+                if (url.Contains(proxystr)) return;
+                textBox2.Text += proxystr;
+            }
+            else
+            {
+                textBox2.Text = textBox2.Text.Replace(proxystr, "");
+            }
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = listBox1.Text;
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            string ss = listBox1.Text;
+            if (ss.Length == 0) return;
+            ShellExecute(IntPtr.Zero, "open", "cmd", "/c " + ss, null, ShowWindowCommands.SW_SHOWNORMAL);
+        }
+        static string urlfilename = Application.StartupPath + "\\urldata.txt";
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            FileStream fs = new FileStream(urlfilename, FileMode.Create, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);  //保存文本的位置在Debug文件夹下
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                sw.Write(listBox1.Items[i]);
+                sw.WriteLine();
+            }
+
+            sw.Close();
+
+        }
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            string str;
+            if (!File.Exists(urlfilename)) return;
+        
+
+            StreamReader sr = new StreamReader(urlfilename, false);
+            while((str = sr.ReadLine()) != null)
+            {
+                listBox1.Items.Add(str);
+            }
+            sr.Close();
+        }
+        #region Message Process
+        //Override WndProc to get messages...
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case WM_DRAWCLIPBOARD:
+                    {
+                        //The clipboard has changed...
+                        //##########################################################################
+                        // Process Clipboard Here :)........................
+                        //##########################################################################
+                        SendMessage(mNextClipBoardViewerHWnd, m.Msg, m.WParam.ToInt32(), m.LParam.ToInt32());
+
+                        //显示剪贴板中的文本信息
+                        if (Clipboard.ContainsText())
+                        {
+                            string ss = Clipboard.GetText();
+                            if (!ss.Contains("http")) return;
+                            textBox1.Text = ss;
+                          //  MessageBox.Show("ddd");
+                        }
+                        //显示剪贴板中的图片信息
+                        if (Clipboard.ContainsImage())
+                        {
+         
+                        }
+                        break;
+                    }
+                case WM_CHANGECBCHAIN:
+                    {
+                        //Another clipboard viewer has removed itself...
+                        if (m.WParam == (IntPtr)mNextClipBoardViewerHWnd)
+                        {
+                            mNextClipBoardViewerHWnd = m.LParam;
+                        }
+                        else
+                        {
+                            SendMessage(mNextClipBoardViewerHWnd, m.Msg, m.WParam.ToInt32(), m.LParam.ToInt32());
+                        }
+                        break;
+                    }
+            }
+            base.WndProc(ref m);
+        }
+        #endregion
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string url = textBox1.Text.ToString();
+            if (url.Length == 0) return;
+            if (!url.Contains("http")) return;
+            string str = "youtube-dl -f best " + url;
+
+            if (checkBox1.Checked) str += " --proxy 127.0.0.1:1080";
+            textBox2.Text = str;
+
+            AddUrl(str);
+        }
+        private bool IsUrlValue(string url)
+        {
+            if (url.Length == 0) return false;
+            if (!url.Contains("http")) return false;
+            if (url.Contains("youtube")) return true;
+            if (url.Contains("chaturbate")) return true;
+            if (url.Contains("pornhub")) return true;
+            return false;
+        }
+        private void AddUrl(string url)
+        {
+
+            if (!IsUrlValue(url)) return;
+
+            if (!listBox1.Items.Contains(url))
+            {
+                listBox1.Items.Add(url);
+                ShellExecute(IntPtr.Zero, "open", "cmd", "/c " + url, null, ShowWindowCommands.SW_SHOWNORMAL);
+            }
+
+            listBox1.SetSelected(listBox1.FindString(url), true);
+        }
+    }
+}
